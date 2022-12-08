25aed7ae872699dfaeacb9d00ca2ff0a844a19be
Saradhi <saradhi0001069@gmail.com>
2022-12-08 18:49:05 +0530

    contact button update

--- components/WaveCta.tsx
+++ components/WaveCta.tsx
@@ -19,7 +19,7 @@ export default function WaveCta() {
           d="M0,64L80,58.7C160,53,320,43,480,80C640,117,800,203,960,197.3C1120,192,1280,96,1360,48L1440,0L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
         ></path>
       </svg> */}
-      <CtaWrapper>
+      <CtaWrapper id="contact">
         <Container>
           <Title>Connect With Us</Title>
           <Description>
